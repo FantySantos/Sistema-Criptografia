3ee70660cb494f9142c371990aa12ea7479ca675
Fantiny Santos <76068925+FantySantos@users.noreply.github.com>
2022-11-02 18:18:21 -0300

    cadastro/login usuário

--- cadastro_usuario.py
+++ cadastro_usuario.py
@@ -11,21 +11,40 @@ def cadastro():
             banco = sqlite3.connect('banco_cadastro.db') 
             cursor = banco.cursor()
             cursor.execute("CREATE TABLE IF NOT EXISTS cadastro (nome text,login text,senha text)")
-            cursor.execute("INSERT INTO cadastro VALUES ('"+nome+"','"+login+"','"+senha+"')")
-
-            banco.commit() 
+            cursor.execute(f"INSERT INTO cadastro VALUES ('{nome}','{login}','{senha}')")
+            banco.commit()
             banco.close()
-            print("Usuario cadastrado com sucesso")
+
+            print("Usuario cadastrado com sucesso.")
 
         except sqlite3.Error as erro:
             print("Erro ao inserir os dados.", erro)
     else:
-        print("As senhas digitadas estão diferentes")
+        print("As senhas digitadas estão diferentes.")
 
+def login():
+    usuario = input('Nome de usuário: ')
+    senha = input('Senha: ')
+    
+    try:
+        banco = sqlite3.connect('banco_cadastro.db') 
+        cursor = banco.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS cadastro (nome text,login text,senha text)")
+        cursor.execute(f"SELECT senha FROM cadastro WHERE (login='{usuario}')")
+        senha_db = cursor.fetchall()
+        banco.close()
 
+        if senha == senha_db[0][0]:
+            print('Você está logado!')
+        else:
+            print('Senha incorreta.')
+    
+    except:
+        print('Nome de usuário não cadastrado.')
+        
 def menu():
     op = int(input('''
-            Bem vinde!!
+            Bem vindo!!
 
         Escolha uma opção:
     [1] Cadastrar usuário.
@@ -33,6 +52,7 @@ def menu():
     [3] Sair.
         
     '''))
+
     return op
 
 def controle():
@@ -41,15 +61,12 @@ def controle():
     while controle != 3:
         if controle == 1:
             cadastro()
-
         elif controle == 2:
-            pass
-
+            login()
         else:
             print('Valor inválido.')
-        
         controle = menu()
-    
+
     print('Programa finalizado!')
 
 controle()
